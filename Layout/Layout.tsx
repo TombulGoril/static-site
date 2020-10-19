77e0a022f9bccc554a57b98c3941bc81efa8f382
dodiameer <mtxshiftg@gmail.com>
2020-10-19 19:53:24 +0300

    Layout - Highlight current page link in nav

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -3,35 +3,63 @@
 import { NextPage } from "next";
 import { ReactNode } from "react";
 import Link from "next/link";
+import styles from "./Layout.module.css";
+import { useRouter } from "next/router";
 
 const Layout: NextPage<{ children: ReactNode; logoWidthHeight?: number }> = ({
   children,
   logoWidthHeight,
 }) => {
+  const Router = useRouter();
   logoWidthHeight = logoWidthHeight || 128;
   return (
     <>
-      <nav>
+      <nav className={styles.nav}>
         <img
-          src="/logo.jpeg"
+          src='/logo.jpeg'
           width={logoWidthHeight}
           height={logoWidthHeight}
-          alt="Tombul Goril Logo"
+          alt='Tombul Goril Logo'
+          className={styles.nav_logo}
         />
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
+        <ul className={styles.nav_list}>
+          <li className={styles.nav_list_item}>
+            <Link href='/'>
+              <a
+                className={`${styles.nav_list_item_link} ${
+                  Router.pathname === "/"
+                    ? styles.nav_list_item_link_current
+                    : ""
+                }`}
+              >
+                Home
+              </a>
             </Link>
           </li>
-          <li>
-            <Link href="/#">
-              <a>About</a>
+          <li className={styles.nav_list_item}>
+            <Link href='/about'>
+              <a
+                className={`${styles.nav_list_item_link} ${
+                  Router.pathname === "/about"
+                    ? styles.nav_list_item_link_current
+                    : ""
+                }`}
+              >
+                About
+              </a>
             </Link>
           </li>
-          <li>
-            <Link href="/#">
-              <a>Products</a>
+          <li className={styles.nav_list_item}>
+            <Link href='/products'>
+              <a
+                className={`${styles.nav_list_item_link} ${
+                  Router.pathname === "/products"
+                    ? styles.nav_list_item_link_current
+                    : ""
+                }`}
+              >
+                Products
+              </a>
             </Link>
           </li>
         </ul>
