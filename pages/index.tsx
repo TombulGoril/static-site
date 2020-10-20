fa786b0c0ee076e03d11f14e18a41556938f6dd8
dodiameer <mtxshiftg@gmail.com>
2020-10-20 15:04:18 +0300

    Home - Finalized slider

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,33 +4,37 @@ import Layout from "../Layout/Layout";
 import ImageGallery from "react-image-gallery";
 
 const Home: NextPage = () => {
+  const sliderImages = [
+    {
+      thumbnail: "/logo.jpeg",
+      original: "/logo.jpeg",
+    },
+    {
+      thumbnail: "/logo.jpeg",
+      original: "/logo.jpeg",
+    },
+    {
+      thumbnail: "/logo.jpeg",
+      original: "/logo.jpeg",
+    },
+    {
+      thumbnail: "/logo.jpeg",
+      original: "/logo.jpeg",
+    },
+    {
+      thumbnail: "/logo.jpeg",
+      original: "/logo.jpeg",
+    },
+  ];
   return (
     <Layout>
       <PageTitle styleName='large'>Tombul Goril</PageTitle>
       <ImageGallery
-        items={[
-          {
-            thumbnail: "/logo.jpeg",
-            original: "/logo.jpeg",
-          },
-          {
-            thumbnail: "/logo.jpeg",
-            original: "/logo.jpeg",
-          },
-          {
-            thumbnail: "/logo.jpeg",
-            original: "/logo.jpeg",
-          },
-          {
-            thumbnail: "/logo.jpeg",
-            original: "/logo.jpeg",
-          },
-          {
-            thumbnail: "/logo.jpeg",
-            original: "/logo.jpeg",
-          },
-        ]}
-      ></ImageGallery>
+        items={sliderImages}
+        showPlayButton={false}
+        showFullscreenButton={false}
+        showThumbnails={false}
+      />
     </Layout>
   );
 };
