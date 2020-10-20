59fb0476a54fd22415db594ffb3c9140b28583d3
dodiameer <mtxshiftg@gmail.com>
2020-10-20 14:56:02 +0300

    Home - Initial slider placeholder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,36 @@
 import { NextPage } from "next";
 import PageTitle from "../Components/PageTitle";
 import Layout from "../Layout/Layout";
+import ImageGallery from "react-image-gallery";
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <PageTitle styleName='large'>Tombul Goril</PageTitle>
+      <ImageGallery
+        items={[
+          {
+            thumbnail: "/logo.jpeg",
+            original: "/logo.jpeg",
+          },
+          {
+            thumbnail: "/logo.jpeg",
+            original: "/logo.jpeg",
+          },
+          {
+            thumbnail: "/logo.jpeg",
+            original: "/logo.jpeg",
+          },
+          {
+            thumbnail: "/logo.jpeg",
+            original: "/logo.jpeg",
+          },
+          {
+            thumbnail: "/logo.jpeg",
+            original: "/logo.jpeg",
+          },
+        ]}
+      ></ImageGallery>
     </Layout>
   );
 };
